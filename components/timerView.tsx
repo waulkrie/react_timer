5467b2508b9015093573ffe524437c4771f4236a
waulkrie <aobare@gmail.com>
2024-03-16 14:24:21 -0500

    fix: replace button with pressable

--- components/timerView.tsx
+++ components/timerView.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { Text, View, Button, ScrollView } from 'react-native';
+import { Text, View, Pressable, ScrollView } from 'react-native';
 import { Link } from 'expo-router';
 import Timer from './timer';
 import ListView from './listView';
@@ -7,8 +7,9 @@ import { styled } from 'nativewind';
 import Accordion from './accordian';
 
 const StyledView = styled(View);
+const StyledText = styled(Text);
 const StyledScrollView = styled(ScrollView);
-const StyledButton = styled(Button);
+const StyledButton = styled(Pressable);
 
 function TimerView() {
   const [isActive, setIsActive] = React.useState(false);
@@ -24,33 +25,38 @@ function TimerView() {
       <StyledView className='mt-8 flex-row justify-center'>
         <Timer started={isActive} time={currentTime} callback={handleUpdateTime} />
       </StyledView>
-      <StyledView className='mt-4 w-full flex-row justify-center'>
+      <StyledView className='mt-4 flex-row justify-center'>
         <StyledButton
-          title={isActive ? 'Stop' : 'Start'}
+          className='rounded-md bg-blue-500 px-2'
           onPress={() => {
             setIsActive(!isActive);
             if (!isActive) {
               setSplitList([]); // clear splitList
             }
           }}
-        />
+        >
+          <StyledText className='text-lg text-white'>{isActive ? 'Stop' : 'Start'}</StyledText>
+        </StyledButton>
         <StyledButton
-          className='bg-blue-500'
-          title='Split'
+          className='rounded-md bg-blue-500 px-2'
           onPress={() => {
             const newList = [...splitList, currentTime];
             setSplitList(newList); // append new split time to splitList
             console.log(newList);
           }}
-        />
+        >
+          <StyledText className='text-lg text-white'>{'Split'}</StyledText>
+        </StyledButton>
         <StyledButton
-          title='Reset Splits'
+          className='rounded-md bg-blue-500 px-2'
           onPress={() => {
             setSplitList([...[]]); // append new split time to splitList
             setCurrentTime('00.000');
             //console.log([...splitList, currentTime]);
           }}
-        />
+        >
+          <StyledText className='text-lg text-white'>{'Reset Splits'}</StyledText>
+        </StyledButton>
       </StyledView>
       {/* <Accordion sections={(Title: 'Title', content: 'hello world')} /> */}
       <StyledScrollView className='max-h-90 mt-4 w-full'>
