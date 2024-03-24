5cd8918f356acaaaca6d1b27d8d364f924d82131
Trenton Bailey <tjb63@students.uwf.edu>
2024-03-23 22:10:22 -0500

    Update profile.tsx
    In progress working on the fetch response and getting the userid from it.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,51 +1,67 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Text } from 'react-native';
+import { styled } from 'nativewind';
+
+type account = {
+    id: string,
+    username: string,
+    email: string,
+}
+
+const StyledView = styled(View);
+const StyledTextInput = styled(TextInput);
 
 
 export default function Login() {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [userid, setUserid] = useState<string>();
 
-  const handleLogin = async () => {
-    try {
-      // 	http://localhost:8081/react-timer.vercel.com/login
-      const response = await fetch('react-timer.vercel.com/login', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 
-          username: username,
-          password: password,
-        }),
-      }).then((res) => {
-        // res.json();
-        console.log('Login successful', res.json());
-      });
-
-    } catch (error) {
-      // Handle login error
-      console.error('Login failed', error);
+    function LoginHelper() {
+        return (
+            <StyledView className="flex-1 justify-start items-center px-4 bg-black">
+                <StyledTextInput
+                    className='text-lg text-blue-500'
+                    placeholder="user@email.com"
+                    value={username}
+                    onChangeText={(text) => setUsername(text)}
+                />
+                <StyledTextInput
+                    className='text-lg text-blue-500'
+                    placeholder="password"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+                <Button title="Login" onPress={handleLogin} />
+            </StyledView>
+        )
     }
-  };
 
-  return (
-    <View className="flex-1 justify-start items-center px-4 bg-black">
-      <TextInput
-        className='text-lg text-blue-500'
-        placeholder="user@email.com"
-        value={username}
-        onChangeText={(text) => setUsername(text)}
-      />
-      <TextInput
-        className='text-lg text-blue-500'
-        placeholder="password"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <Button title="Login" onPress={handleLogin} />
-    </View>
-  );
+    const handleLogin = async () => {
+        try {
+            // 	https://potential-waffle-p59v4w665pqf4vw-80.app.github.dev/login
+            const response = await fetch('potential-waffle-p59v4w665pqf4vw-80.app.github.dev/login', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            });
+            const data: account = await response.json();
+            console.log('Login Successful', data);
+
+        } catch (error) {
+            // Handle login error
+            console.error('Login failed', error);
+        }
+    };
+
+    return (
+        LoginHelper()
+    );
 };
