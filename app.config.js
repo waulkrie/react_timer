7895c4ebcee8441b94b574e52f4542bd1cc7ee9f
O'neal Barre <waulkrie@users.noreply.github.com>
2024-03-28 20:40:31 -0500

    feat:clerk signup added

---
+++ app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'React-Timer-App',
+  version: '1.0.0',
+  extra: {
+    clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+  },
+};
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,67 +1,49 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Text } from 'react-native';
 import { styled } from 'nativewind';
-
-type account = {
-    id: string,
-    username: string,
-    email: string,
-}
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import SignUpScreen from 'components/SignUpScreen';
 
 const StyledView = styled(View);
 const StyledTextInput = styled(TextInput);
-
+const StyledText = styled(Text);
 
 export default function Login() {
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [userid, setUserid] = useState<string>();
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-    function LoginHelper() {
-        return (
-            <StyledView className="flex-1 justify-start items-center px-4 bg-black">
-                <StyledTextInput
-                    className='text-lg text-blue-500'
-                    placeholder="user@email.com"
-                    value={username}
-                    onChangeText={(text) => setUsername(text)}
-                />
-                <StyledTextInput
-                    className='text-lg text-blue-500'
-                    placeholder="password"
-                    secureTextEntry
-                    value={password}
-                    onChangeText={(text) => setPassword(text)}
-                />
-                <Button title="Login" onPress={handleLogin} />
-            </StyledView>
-        )
+  const handleLogin = async () => {
+    try {
+      // 	http://localhost:8081/react-timer.vercel.com/login
+      const response = await fetch('https://heroku.app', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      }).then((res) => {
+        // res.json();
+        console.log('Login successful', res.json());
+      });
+    } catch (error) {
+      // Handle login error
+      console.error('Login failed', error);
     }
+  };
 
-    const handleLogin = async () => {
-        try {
-            // 	https://potential-waffle-p59v4w665pqf4vw-80.app.github.dev/login
-            const response = await fetch('potential-waffle-p59v4w665pqf4vw-80.app.github.dev/login', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: username,
-                    password: password,
-                }),
-            });
-            const data: account = await response.json();
-            console.log('Login Successful', data);
-
-        } catch (error) {
-            // Handle login error
-            console.error('Login failed', error);
-        }
-    };
-
-    return (
-        LoginHelper()
-    );
-};
+  return (
+    <StyledView className='flex-1 items-center justify-start bg-zinc-200 px-4'>
+      <SignedIn>
+        <StyledText className='text-red-600'>You are Signed in</StyledText>
+      </SignedIn>
+      <SignedOut>
+        <StyledText className='text-red-600'>You are Signed out, please login or sign up</StyledText>
+        <SignUpScreen />
+      </SignedOut>
+    </StyledView>
+  );
+}
